43893a8f199c96864b0817164fc0d23632546497
Joey Wan <joeyhongjiwan@gmail.com>
2024-05-06 20:54:19 -0400

    completed basic prediction of weather

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from datetime import datetime as dt
+from datetime import date
+import matplotlib.pyplot as plt
+import torch
 
 d = {}
 with open("climate.csv") as file:
     #skipping the first line
     next(file)
 
-    #Just to limite the dataset a bit
-    n = 0
-    
-    
+
     #Reading each line in csv 
     for line in file:
         # data is formatted as [date, max temp(farenheit)]
@@ -17,18 +17,23 @@ with open("climate.csv") as file:
         
         #Skip missing data
         if data[1].strip() != "M":
-            date = data[0]
-            date = dt.strptime(date, '%m/%d/%Y').timetuple().tm_yday
+            time = data[0]
+            time = dt.strptime(time, '%m/%d/%Y').timetuple().tm_yday
             # set value as list to append to each time with new temperatures
-            if d.get(date) == None:
-                d[date] = d.get(date, [temp])
-            d.get(date).append(temp) 
-        
-        #Just to limit the dataset a bit
-        n += 1
-        if n == 1000:
-            break
+            if d.get(time) == None:
+                d[time] = d.get(time, [temp])
+            d.get(time).append(temp) 
+       
 
+
+# key is 1 to 365, value is list of temperatures 
 d = dict(sorted(d.items()))
-print(d)
+T = torch.zeros((1, 366), dtype=torch.int32)
+for day, temperatures in d.items():
+    average_temp = round(sum([int(temperature) for temperature in temperatures])/len(temperatures))
+    # it starts indexed at zero, 0-365, but has the len is 366
+    T[0, day - 1] = average_temp
 
+current = date.today().timetuple().tm_yday
+today_temperature = T[0, current-1].item()
+print(today_temperature)
